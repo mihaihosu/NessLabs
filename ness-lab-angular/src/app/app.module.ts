8e9c1a52d74bb534a3c350070b50516625870981
Calin Ovidiu <calinovidiu15@yahoo.ro>
2023-04-24 18:04:58 +0300

    Revert "admin-login-v1"
    This reverts commit c71a73d9bca1c8a3ff420219768487e71b0021f7.

--- ness-lab-angular/src/app/app.module.ts
+++ ness-lab-angular/src/app/app.module.ts
@@ -1,23 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatIconModule} from '@angular/material/icon';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LoginAdminComponent } from './login-admin/login-admin.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
+
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginAdminComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    MatIconModule,
-    ReactiveFormsModule,
-    MatFormFieldModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
