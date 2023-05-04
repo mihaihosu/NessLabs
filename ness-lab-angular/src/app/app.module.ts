db1dde1000d06254ae9c5e4b07371e9ca60680af
Calin Ovidiu <calinovidiu15@yahoo.ro>
2023-05-05 01:02:49 +0300

    created page layout

--- ness-lab-angular/src/app/app.module.ts
+++ ness-lab-angular/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventCardsComponent } from './event-cards/event-cards.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
-  declarations: [AppComponent, EventCardsComponent],
+  declarations: [AppComponent, EventCardsComponent, LayoutComponent],
   imports: [BrowserModule, AppRoutingModule, MatIconModule],
   providers: [],
   bootstrap: [AppComponent],
