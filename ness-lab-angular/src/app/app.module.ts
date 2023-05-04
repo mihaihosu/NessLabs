f63a42aba7b55eaec4c48857ada2975c71780ef8
magdapaicu <102292971+magdapaicu@users.noreply.github.com>
2023-05-04 23:07:06 +0300

    event-cards

--- ness-lab-angular/src/app/app.module.ts
+++ ness-lab-angular/src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventCardsComponent } from './event-cards/event-cards.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, EventCardsComponent],
+  imports: [BrowserModule, AppRoutingModule, MatIconModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ ness-lab-angular/src/app/event-cards/event-cards.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-cards',
+  templateUrl: './event-cards.component.html',
+  styleUrls: ['./event-cards.component.scss'],
+})
+export class EventCardsComponent implements OnInit {
+  constructor() {}
+
+  events = {
+    events: [
+      {
+        data: '30 MARTIE 2023',
+        ora: '20:00',
+        titlu: 'Concert Chitara',
+        loc: 'Piata Unirii',
+        autor: 'OtherAdminName',
+        imgURL: './assets/img/img1.png',
+      },
+      {
+        data: '03 OCTOMBRIE 2023',
+        ora: '20:00',
+        titlu: 'Concert Chitara',
+        loc: 'Piata Unirii',
+        autor: 'OtherAdminName',
+        imgURL: './assets/img/img2.png',
+      },
+      {
+        data: '13 FEBRUARIE 2023',
+        ora: '20:00',
+        titlu: 'Concert Chitara',
+        loc: 'Piata Unirii',
+        autor: 'OtherAdminName',
+        imgURL: './assets/img/img3.png',
+      },
+      {
+        data: '15 APRILIE 2023',
+        ora: '20:00',
+        titlu: 'Concert Chitara',
+        loc: 'Piata Unirii',
+        autor: 'OtherAdminName',
+        imgURL: './assets/img/img4.png',
+      },
+      {
+        data: '12 IUNIE 2023',
+        ora: '20:00',
+        titlu: 'Concert Chitara',
+        loc: 'Piata Unirii',
+        autor: 'OtherAdminName',
+        imgURL: './assets/img/img1.png',
+      },
+      {
+        data: '15 APRILIE 2023',
+        ora: '20:00',
+        titlu: 'Concert Chitara',
+        loc: 'Piata Unirii',
+        autor: 'OtherAdminName',
+        imgURL: './assets/img/img4.png',
+      },
+      {
+        data: '12 IUNIE 2023',
+        ora: '20:00',
+        titlu: 'Concert Chitara',
+        loc: 'Piata Unirii',
+        autor: 'OtherAdminName',
+        imgURL: './assets/img/img1.png',
+      },
+      {
+        data: '13 FEBRUARIE 2023',
+        ora: '20:00',
+        titlu: 'Concert Chitara',
+        loc: 'Piata Unirii',
+        autor: 'OtherAdminName',
+        imgURL: './assets/img/img3.png',
+      },
+    ],
+  };
+
+  ngOnInit(): void {}
+}
