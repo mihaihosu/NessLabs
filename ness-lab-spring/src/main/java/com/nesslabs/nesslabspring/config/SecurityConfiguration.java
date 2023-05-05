0c844ef2224a4a642a9172b22542b360a0585fff
SuperxJoker <oaidaalexia@yahoo.com>
2023-05-05 17:48:53 +0300

    Changed origin in cors

--- ness-lab-spring/src/main/java/com/nesslabs/nesslabspring/config/SecurityConfiguration.java
+++ ness-lab-spring/src/main/java/com/nesslabs/nesslabspring/config/SecurityConfiguration.java
@@ -26,7 +26,7 @@ public class SecurityConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost", "http://localhost:4200")
+                .allowedOrigins("http://localhost:8080", "http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
