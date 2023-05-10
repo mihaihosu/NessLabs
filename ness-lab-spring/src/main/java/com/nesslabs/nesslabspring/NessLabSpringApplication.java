2bffe6320e67f8f3bb6b3429b8ca8e5fba9f9a5e
AnaMaghear <ana.maghear@yahoo.com>
2023-05-10 22:32:29 +0300

    Added swagger

--- ness-lab-spring/src/main/java/com/nesslabs/nesslabspring/NessLabSpringApplication.java
+++ ness-lab-spring/src/main/java/com/nesslabs/nesslabspring/NessLabSpringApplication.java
@@ -1,12 +1,15 @@
 package com.nesslabs.nesslabspring;
 
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Library APIS", version = "3.0", description = ""))
 public class NessLabSpringApplication {
 
 	public static void main(String[] args) {
