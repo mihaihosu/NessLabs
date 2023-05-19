f4ad90686efbf9ee8c24744e22ee4096b8b7a814
AnaMaghear <ana.maghear@yahoo.com>
2023-05-19 22:07:36 +0300

    Finished script to add users in h2 database

---
+++ ness-lab-spring/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO _user (email, username, password, is_admin, is_confirmed)
+VALUES ('sara@yahoo.com', 'Sara', 'Password1@', true, false),
+       ('lara@yahoo.com', 'Lara', 'Password2@', false, true),
+       ('cara@yahoo.com', 'Cara', 'Password3@', false, true),
+       ('vara@yahoo.com', 'Vara', 'Password4@', true, true),
+       ('para@yahoo.com', 'Para', 'Password5@', false, false),
+       ('iara@yahoo.com', 'Iara', 'Password6@', false, true),
+       ('Tara@yahoo.com', 'Tara', 'Password7@', true, false),
+       ('Oara@yahoo.com', 'Oara', 'Password8@', false, true);
