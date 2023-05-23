db8f44d50e03da7caa044133eab818255a4e9a3b
alexbhr13 <73994505+alexbhr13@users.noreply.github.com>
2023-05-23 22:53:47 +0300

    created endpoint, event service and event validator are wip, minor other adjustments

--- ness-lab-spring/src/main/java/com/nesslabs/nesslabspring/service/EventServiceImpl.java
+++ ness-lab-spring/src/main/java/com/nesslabs/nesslabspring/service/EventServiceImpl.java
@@ -7,6 +7,7 @@ import com.nesslabs.nesslabspring.repository.EventRepository;
 import com.nesslabs.nesslabspring.security.JwtService;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
